0c38776adcb27f6ccedceb13ba01dd81c1ee6d2e
dmitrisaberi <64174951+dmitrisaberi@users.noreply.github.com>
2023-03-19 18:53:54 -0700

    more edits

--- automated-interpretation.py
+++ automated-interpretation.py
@@ -1,14 +1,25 @@
 import openai
+import tiktoken
+
+encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+
 openai.api_key = "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"
 context = "is there a rough, overarching theme tying most (i.e., >40%) of the following tokens together? We are using the term “theme” generously here — for example, if most of the outputs are numbers with no discernable pattern, then output “Yes: numbers”. This could also be a more broad one such as “religion”. There could also be two semantic meanings of a word that are both captured by the model — in this case, please provide both meanings.Please give your answer in the format 'Yes: (insert less than 4 word summary of concept)' or 'No', with no explanation."
 def get_interpretability(act_prompts):
     interpretable_neurons = {}
+    flag = False
     for neur in act_prompts.keys():
+        print(neur)
         token_string = ": "
         for key in act_prompts[neur].keys():
             for token in act_prompts[neur][key]:
                 token_string = token_string + ", " + token
-        print(len(token_string.split()) + len(context.split()))
+                if len(encoding.encode(context + token_string)) > 4050:
+                    flag = True
+                    break
+            if flag:
+                break
+        print(len(encoding.encode(context + token_string)))
         response = openai.ChatCompletion.create(
           model="gpt-3.5-turbo",
           messages=[
@@ -17,9 +28,10 @@ def get_interpretability(act_prompts):
             ]
         )
         interpretation = response['choices'][0]['message']['content']
+        print(token_string)
         print(interpretation)
-        if "I'm sorry" in interpretation:
-            print(token_string)
+       # if "I'm sorry" in interpretation:
+           # print(token_string)
         if "Yes" in interpretation:
-            print(interpretation[4:])
+           # print(interpretation[4:])
             interpretable_neurons[neur] = interpretation[4:]
