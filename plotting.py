781e7e511a23d7f620aab1b679a6d39bd131b7f6
dmitrisaberi <64174951+dmitrisaberi@users.noreply.github.com>
2023-03-20 08:56:24 -0700

    adding encoder 11 layer

--- plotting.py
+++ plotting.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pickle
 
 def plot_topk(layer_number, encoder=True):
@@ -13,4 +14,24 @@ def plot_topk(layer_number, encoder=True):
 
     counts, bins, bars = plt.hist(values, bins=range(3072)) #, log=True)
     plt.title("Top-" + str(k) + " neuron presence; Encoder layer " + str(layer_number))
+    plt.show()
+
+def plot_layer_sparsities():
+    with open("squad_layer_sparsity.pkl", "rb") as fp:
+        sparsities = pickle.load(fp)
+    avg_sparsities = {}
+    for key in sparsities.keys():
+        s = np.array(sparsities[key])
+        avg_sparsities[key] = np.mean(s)   
+#    names = list(avg_sparsities.keys())
+    values = list(avg_sparsities.values())
+    shortened_names = []
+    for i in range(24):
+        if i < 12:
+            shortened_names.append("e" + str(i))
+        else:
+            shortened_names.append("e" + str(i%12))
+    plt.figure(figsize=(10,6))
+    plt.bar(range(len(avg_sparsities)), values, tick_label=shortened_names)
+    plt.title("Average percentage nonzeros of activation layers")
     plt.show()
