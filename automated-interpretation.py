7d3d1a676a952e4d5a652337495757f2add90c42
dmitrisaberi <64174951+dmitrisaberi@users.noreply.github.com>
2023-03-19 18:10:19 -0700

    modifying automated interpretation

--- automated-interpretation.py
+++ automated-interpretation.py
@@ -1,13 +1,14 @@
 import openai
-openai.api_key = "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"
+openai.api_key = "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"
 context = "is there a rough, overarching theme tying most (i.e., >40%) of the following tokens together? We are using the term “theme” generously here — for example, if most of the outputs are numbers with no discernable pattern, then output “Yes: numbers”. This could also be a more broad one such as “religion”. There could also be two semantic meanings of a word that are both captured by the model — in this case, please provide both meanings.Please give your answer in the format 'Yes: (insert less than 4 word summary of concept)' or 'No', with no explanation."
 def get_interpretability(act_prompts):
     interpretable_neurons = {}
     for neur in act_prompts.keys():
+        token_string = ": "
         for key in act_prompts[neur].keys():
-            token_string = ": "
             for token in act_prompts[neur][key]:
                 token_string = token_string + ", " + token
+        print(len(token_string.split()) + len(context.split()))
         response = openai.ChatCompletion.create(
           model="gpt-3.5-turbo",
           messages=[
@@ -16,5 +17,9 @@ def get_interpretability(act_prompts):
             ]
         )
         interpretation = response['choices'][0]['message']['content']
+        print(interpretation)
+        if "I'm sorry" in interpretation:
+            print(token_string)
         if "Yes" in interpretation:
+            print(interpretation[4:])
             interpretable_neurons[neur] = interpretation[4:]
