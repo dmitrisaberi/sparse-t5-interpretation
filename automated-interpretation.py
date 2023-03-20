a4dec17fa0cbb8fa22f35e164be1e34772a1ef60
dmitrisaberi <64174951+dmitrisaberi@users.noreply.github.com>
2023-03-19 20:05:46 -0700

    fixing flag palcement

--- automated-interpretation.py
+++ automated-interpretation.py
@@ -3,13 +3,12 @@ import tiktoken
 
 encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
 
-openai.api_key = "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"
-context = "is there a rough, overarching theme tying most (i.e., >40%) of the following tokens together? We are using the term “theme” generously here — for example, if most of the outputs are numbers with no discernable pattern, then output “Yes: numbers”. This could also be a more broad one such as “religion”. There could also be two semantic meanings of a word that are both captured by the model — in this case, please provide both meanings.Please give your answer in the format 'Yes: (insert less than 4 word summary of concept)' or 'No', with no explanation."
+openai.api_key = "API-KEY-HERE"
+context = "is there a rough, overarching theme tying most (i.e., >40%) of the following tokens together? We are using the term “theme” generously here — for example, if most of the outputs are numbers with no discernable pattern, then output “Yes: numbers”. This could also be a more broad one such as “religion”. Please give your answer in the format 'Yes: (insert less than 4 word summary of concept)' or 'No', with no explanation."
 def get_interpretability(act_prompts):
     interpretable_neurons = {}
-    flag = False
     for neur in act_prompts.keys():
-        print(neur)
+        flag = False
         token_string = ": "
         for key in act_prompts[neur].keys():
             for token in act_prompts[neur][key]:
